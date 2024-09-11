1e608746139f2492c534248c463565380f214e94
timy-13 <113146011+timy-13@users.noreply.github.com>
2024-09-10 18:29:54 -0600

    Create HUD to show countdown timer and player money, implement money calculations

--- Source/NoTimeToWait/NoTimeToWaitGameMode.cpp
+++ Source/NoTimeToWait/NoTimeToWaitGameMode.cpp
@@ -4,6 +4,8 @@
 #include "NoTimeToWaitCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
+#include "NTTWGameplayTags.h"
+
 ANoTimeToWaitGameMode::ANoTimeToWaitGameMode()
 {
 	// set default pawn class to our Blueprinted character
@@ -11,5 +13,91 @@ ANoTimeToWaitGameMode::ANoTimeToWaitGameMode()
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
+
+void ANoTimeToWaitGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
+{
+	Super::InitGame(MapName, Options, ErrorMessage);
+
+	Difficulty Easy;
+	Easy.CustomerNumber = 4;
+	Easy.TotalTime = 120;
+	Easy.OneStarMoney = 100;
+	Easy.TwoStarMoney = 140;
+	Easy.ThreeStarMoney = 200;
+	DifficultyRules.Add(Easy);
+
+	CustomerType Normal;
+	Normal.Type = NTTWGameplayTags::TAG_Customer_Normal;
+	Normal.MenuTime = 10.f;
+	Normal.ServingTime = 10.f;
+	Normal.EatingTime = 10.f;
+	Normal.Money = 20;
+	CustomerTypes.Add(Normal.Type, Normal);
+}
+
+void ANoTimeToWaitGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	SetDifficulty(0);
+
+	RemainingMinutes = CurrentDifficulty.TotalTime / 60;
+	RemainingSeconds = CurrentDifficulty.TotalTime % 60;
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ANoTimeToWaitGameMode::CountDown, 1.0f, true, 0.f);
+
+}
+
+void ANoTimeToWaitGameMode::EndLevel()
+{
+	// stop player input
+	// display endlevel widget
+}
+
+Difficulty ANoTimeToWaitGameMode::GetDifficulty() const
+{
+	return CurrentDifficulty;
+}
+
+void ANoTimeToWaitGameMode::SetDifficulty(const int Index)
+{
+	if (DifficultyRules.Num() > Index)
+	{
+		CurrentDifficulty =  DifficultyRules[Index];
+	}
+}
+
+CustomerType ANoTimeToWaitGameMode::GetCustomerType(const FGameplayTag& Type) const
+{
+	return CustomerTypes[Type];
+}
+
+void ANoTimeToWaitGameMode::UpdatePlayerMoney(const float Money)
+{
+	PlayerMoney += Money;
+}
+
+float ANoTimeToWaitGameMode::GetPlayerMoney() const
+{
+	return PlayerMoney;
+}
+
+void ANoTimeToWaitGameMode::CountDown()
+{
+	if (RemainingSeconds > 0)
+	{
+		RemainingSeconds--;
+	}
+	else if (RemainingMinutes > 0)
+	{
+		RemainingMinutes--;
+		RemainingSeconds = 59;
+	}
+	else
+	{
+		GetWorld()->GetTimerManager().ClearTimer(TimerHandle);
+		EndLevel();
 	}
 }
--- Source/NoTimeToWait/NoTimeToWaitGameMode.h
+++ Source/NoTimeToWait/NoTimeToWaitGameMode.h
@@ -4,7 +4,28 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "GameplayTagContainer.h"
 #include "NoTimeToWaitGameMode.generated.h"
+
+struct Difficulty
+{
+	int CustomerNumber;
+	int TotalTime;
+	float OneStarMoney;
+	float TwoStarMoney;
+	float ThreeStarMoney;
+};
+
+struct CustomerType
+{
+	FGameplayTag Type;
+	float MenuTime; // Max time to wait for menu
+	float ServingTime; // Max time to be served
+	float EatingTime;
+	float Money;
+};
+
+class TableHandler;
 
 UCLASS(minimalapi)
 class ANoTimeToWaitGameMode : public AGameModeBase
@@ -13,6 +34,41 @@ class ANoTimeToWaitGameMode : public AGameModeBase
 
 public:
 	ANoTimeToWaitGameMode();
+
+	virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
+
+	virtual void StartPlay() override;
+	void EndLevel();
+
+	Difficulty GetDifficulty() const;
+	void SetDifficulty(const int Index);
+
+	CustomerType GetCustomerType(const FGameplayTag& Type) const;
+
+	float GetPlayerMoney() const;
+	void UpdatePlayerMoney(const float Money);
+
+
+private:
+	void CountDown();
+
+	TArray<Difficulty> DifficultyRules; // initialize in InitGame
+	// store difficulty rules
+
+	TMap<FGameplayTag, CustomerType> CustomerTypes;
+
+	Difficulty CurrentDifficulty;
+
+	FTimerHandle TimerHandle;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Money", meta = (AllowPrivateAccess = "true"))
+	float PlayerMoney;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Timer", meta = (AllowPrivateAccess = "true"))
+	int RemainingMinutes;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Timer", meta = (AllowPrivateAccess = "true"))
+	int RemainingSeconds;
 };
 
 
